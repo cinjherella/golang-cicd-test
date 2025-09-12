c53236cbb840f374c1946bad1c5c4414b836c539
cinjherella <93306091+cinjherella@users.noreply.github.com>
2025-09-12 16:33:40 +0800

    Update output.go

--- helpers/output.go
+++ helpers/output.go
@@ -1,3 +1,3 @@
 package helpers
 
-const OUTPUT = "CICD in AWS CodeBuild, CodeDeploy and Pipeline Test by Jaineyyy!"
+const OUTPUT = "My First CICD in AWS CodeBuild, CodeDeploy and Pipeline Test by Jaineyyy!"
